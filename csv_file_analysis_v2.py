47d25ae7adf8976bf4d5c7d2283dfe7c3a42c46b
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-09-13 08:15:36 +0300

    Update csv_file_analysis_v2.py

--- csv_file_analysis_v2.py
+++ csv_file_analysis_v2.py
@@ -19,13 +19,16 @@ class Csv_File_Analysis():
         self.filename = ""
         self.menu = Menu(self.master)
         self.file_menu = Menu(self.menu, tearoff=0)
-        self.file_menu.add_command(label="Insert a csv file", accelerator='Ctrl+O', command=self.insertfile)
-        self.file_menu.add_command(label="Close file", accelerator='Ctrl+F4', command=self.closefile)
+        self.file_menu.add_command(label="Insert a csv file",
+                                   accelerator='Ctrl+O', command=self.insertfile)
+        self.file_menu.add_command(label="Close file",
+                                   accelerator='Ctrl+F4', command=self.closefile)
         self.file_menu.add_command(label="Save analysis")
         self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command=self.exitmenu)
         self.menu.add_cascade(label="File", menu=self.file_menu)
         self.show_menu = Menu(self.menu, tearoff=0)
-        self.show_menu.add_command(label="Show analysis", accelerator='Ctrl+F5', command=self.show_analysis)
+        self.show_menu.add_command(label="Show analysis",
+                                   accelerator='Ctrl+F5', command=self.show_analysis)
         self.menu.add_cascade(label="Show", menu=self.show_menu)
         self.about_menu = Menu(self.menu, tearoff=0)
         self.about_menu.add_command(label="About", accelerator='Ctrl+I', command=aboutmenu)
@@ -41,10 +44,11 @@ class Csv_File_Analysis():
         self.master.bind('<Control-F1>', lambda event: helpmenu())
         self.master.bind('<Control-i>', lambda event: aboutmenu())
     def print_analysis(self):
+        """ shows basic info about the csv file """
         msg.showinfo("CSV FILE ANALYSIS", "PATH:"+self.filename + 
-                "\nEMPTY CELLS:"+("Yes" if  self.df.isnull().any else "NO") +
-                "\nDUPLICATES"+("Yes" if self.df.duplicated().any else "NO")+
-                "\nSHAPE:"+ str(self.df.shape) +"\nINDEX:"+str(self.df.index))
+                     "\nEMPTY CELLS:"+("Yes" if  self.df.isnull().any else "NO") +
+                     "\nDUPLICATES"+("Yes" if self.df.duplicated().any else "NO")+
+                     "\nSHAPE:"+ str(self.df.shape) +"\nINDEX:"+str(self.df.index))
     def show_analysis(self):
         """ shows analysis """
         if not ".csv" in self.filename:
@@ -66,7 +70,8 @@ class Csv_File_Analysis():
             msg.showerror("ERROR", "A CSV FILE IS ALREADY OPEN")
         else:
             self.filename = filedialog.askopenfilename(initialdir="/", title="Select csv file",
-                                                       filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+                                                       filetypes=(("csv files", "*.csv"),
+                                                                  ("all files", "*.*")))
             if self.filename.endswith('.csv'):
                 self.df = pd.read_csv(self.filename)
                 msg.showinfo("SUCCESSFUL INSERTION", "YOUR CSV FILE HAS SUCCESFULLY INSERTED")
